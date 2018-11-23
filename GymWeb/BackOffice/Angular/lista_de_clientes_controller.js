aa29a9ca7e73d0d82e6088bfa1fe5681f4f2e165
Pedro Pinheiro <pfpinheiro64@gmail.com>
2018-11-23 16:16:15 +0000

    Página de Gestão de Clientes close #17

--- GymWeb/BackOffice/Angular/lista_de_clientes_controller.js
+++ GymWeb/BackOffice/Angular/lista_de_clientes_controller.js
@@ -8,27 +8,32 @@ app.controller("clientesCtrl", function ($scope, $http) {
         //console.log(response.data);
         $scope.Clientes = response.data;
     }, function myError(response) {
-        $scope.firstName = "Error: Table Failed to Load!"
+        console.log("Falha ao Carregar a Tabela!");
+        $scope.firstName = "Falha ao Carregar a Tabela!"
+        //Gestão de Erros
+        //Validações
     });
 
 
     //Remove o cliente da Base de Dados
     $scope.rmCliente = function (id) {
-        console.log($scope.Clientes);
-        console.log(id);
+        //console.log($scope.Clientes);
+        //console.log(id);
         $http({
             method: "DELETE",
-            url: "https://localhost:5001/api/clients/"+id
+            url: "https://localhost:5001/api/clients/" + id
         }).then(function mySuccess() {
             console.log("Removido com Sucesso!");
-            $scope.Clientes = $.grep($scope.Clientes, function(e){ 
-                return e.id != id; 
-           });
+            $scope.Clientes = $.grep($scope.Clientes, function (e) {
+                return e.id != id;
+            });
         }, function myError(response) {
             console.log("Erro ao remover Cliente");
+
+            //Gestão de Erros
+            //Validações
         });
     };
-
 
     //Abre um popup para colocar nova informação do Cliente
     $scope.edCliente = function (id) {
--- GymWeb/BackOffice/Angular/submissions.js
+++ GymWeb/BackOffice/Angular/submissions.js
@@ -1,10 +1,25 @@
-
+//Age Function - Calcula a idade do user através da sua dob
+function getAge(DOB) {
+    let today = new Date();
+    let birthDate = new Date(DOB);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age = age - 1;
+    }
+    return age;
+}
 
 //CLIENTE
 //Controller que recolhe a informação do Form Client e envia para a API
 app.controller("adicionarCliente", function ($scope, $http) {
     $scope.submit = function () {
-        let data = JSON.stringify($scope.user);
+        //Formata a dob do user
+        let date = new Date($scope.user.birthDate.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+        let user = $scope.user;
+        //Calcula a idade do user através da sua dob
+        user.age = getAge(date);
+        let data = JSON.stringify(user);
         //console.log(data);
         $http({
             method: "POST",
@@ -15,26 +30,36 @@ app.controller("adicionarCliente", function ($scope, $http) {
             }
         }).then(function mySuccess(response) {
             console.log("Cliente adicionado com sucesso!");
-            $scope.Clientes = $scope.Clientes.push(response.data);
+            let resposta = response.data;
+            let list = $scope.Clientes;
+            list.push(resposta);
+            $scope.Clientes = list
+            $('#addClient form :input').val("");
         }, function myError(response) {
             console.log("Erro ao adicionar Cliente!");
+
+            //Gestão de Erros
+            //Validações
         });
 
     };
 });
 
-//Controller que recolhe a informação do Form Client e envia para a API para editar o Cliente
+//Controller que recolhe a informação do Form EdClient copia para um novo cliente e envia para a API para assim editar o Cliente
 app.controller("editarCliente", function ($scope, $http) {
     $scope.edsubmit = function () {
         /*Aviso: Como a notificação e o CheckIn ainda não foram implementados não dá para passar 
         esses atributos.*/
         let oldClient = $scope.Clientes.find(x => x.id === $scope.idclienteedit);
+        //Formata a dob do user
+        let date = new Date(oldClient.birthDate.replace(/(\d{2})-(\d{2})-(\d{4})/, "$2/$1/$3"));
+
         //console.log(oldClient);
         let newClient = oldClient;
             newClient.id = $scope.idclienteedit;
             newClient.firstName = $scope.user.firstName;
-            newClient.lastName  = $scope.user.lastName;
-            newClient.age = $scope.user.age;
+            newClient.lastName = $scope.user.lastName;
+            newClient.age = getAge(date); //Calcula a idade do user através da sua dob
             newClient.birthDate = $scope.user.birthDate;
             newClient.nif = $scope.user.nif;
             newClient.heightInMeters = $scope.user.heightInMeters;
@@ -44,17 +69,32 @@ app.controller("editarCliente", function ($scope, $http) {
         $http({
             method: "PUT",
             data: newClient,
-            url: "https://localhost:5001/api/clients/"+$scope.idclienteedit,
+            url: "https://localhost:5001/api/clients/" + $scope.idclienteedit,
             headers: {
                 'content-type': "application/json"
             }
         }).then(function mySuccess(response) {
             console.log("Cliente editado com sucesso!");
+            $('#editarClient form :input').val("");
+            $('#editarClient').modal('toggle');
         }, function myError(response) {
             console.log("Erro ao adicionar Cliente!");
+
+            //Gestão de Erros
+            //Validações
         });
     };
 });
+
+
+
+
+
+
+
+
+
+
 
 
 
