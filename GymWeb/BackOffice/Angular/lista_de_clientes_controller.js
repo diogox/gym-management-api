8c21311d0b690647982f82b3b70b0ae1ce8303df
Pedro Pinheiro <pfpinheiro64@gmail.com>
2018-12-06 20:00:01 +0000

    Gestão de Erros, Validações de Inputs e Correção de Bugs

--- GymWeb/BackOffice/Angular/lista_de_clientes_controller.js
+++ GymWeb/BackOffice/Angular/lista_de_clientes_controller.js
@@ -1,4 +1,4 @@
-import { getClients, removeClient } from './pedidos.js'
+import { getClients, removeClient, adicionarClient, editarClient } from './pedidos.js'
 
 //Format date to yyyy-mm-dd
 function formatDate(date) {
@@ -13,8 +13,54 @@ function formatDate(date) {
     return [year, month, day].join('-');
 }
 
+//Age Function - Calcula a idade do user através da sua dob
+function getAge(DOB) {
+    let today = new Date();
+    let birthDate = new Date(DOB);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age = age - 1;
+    }
+    return age;
+}
+
 //Controller da Gestão dos Clientes
 app.controller("clientesCtrl", function ($scope, $http) {
+
+    //Estilo do Alerta de Erro
+    $scope.redAlert = {
+        "width": "100%",
+        "color": "white",
+        "background-color": "red"
+    }
+
+    //Estilo do Alerta de Sucesso
+    $scope.greenAlert = {
+        "width": "100%",
+        "color": "white",
+        "background-color": "green"
+    }
+
+
+    //Alertas
+    $scope.alerts = [
+        //Erro ao Adicionar um Cliente Index:0
+        { type: 'Error', msg: 'Erro ao Adicionar o Cliente!', style: $scope.redAlert, show: false },
+        //Sucesso ao Adicionar um Cliente Index:1
+        { type: 'Success', msg: 'Cliente Adicionado com Sucesso!', style: $scope.greenAlert, show: false },
+        //Sucesso ao Editar um Cliente Index:2
+        { type: 'Success', msg: 'Cliente Editado com Sucesso!', style: $scope.greenAlert, show: false },
+        //Erro ao Editar um Cliente Index:3
+        { type: 'Error', msg: 'Erro ao Editar o Cliente!', style: $scope.redAlert, show: false },
+        //Erro ao Carregar a Tabela de Clientes Index:4
+        { type: 'Error', msg: 'Erro ao Carregar a Tabela de Clientes!', style: $scope.redAlert, show: false },
+    ];
+
+    //Fechar Alerta pelo ID
+    $scope.closeAlert = function (index) {
+        $scope.alerts[index].show = false;
+    }
 
     //Listar todos os Clientes
     //Executa a função para pedir os dados à API
@@ -30,13 +76,89 @@ app.controller("clientesCtrl", function ($scope, $http) {
             //Atualiza a Lista de Clientes
             $scope.Clientes = response.data;
         } else {
-            console.log("Falha ao Carregar a Tabela!");
-            $scope.firstName = "Falha ao Carregar a Tabela!"
-
-            //Gestão de Erros
-            //Validações
+            $scope.alerts[4].show = true;
         }
     });
+
+
+    //Adicionar Cliente á Base de Dados
+    $scope.submitADD = function () {
+        //Formata a dob do user
+        let date = new Date($scope.user.birthDate);
+        let user = $scope.user;
+        //Calcula a idade do user através da sua dob
+        user.age = getAge(date);
+        let data = JSON.stringify(user);
+
+        adicionarClient($http, data, (response) => {
+            if (response) {
+                let resposta = response.data;
+
+                //Formata a birthdate do cliente para yyyy-mm-dd
+                resposta.birthDate = formatDate(resposta.birthDate);
+
+                //Atualiza a lista sem dar refresh na pagina
+                let list = $scope.Clientes;
+                list.push(resposta);
+                $scope.Clientes = list
+
+                //Dá reset e close no Modal form
+                $('#addClient form :input').val("");
+                $('#addClient').modal('toggle');
+                $scope.alerts[1].show = true;
+            } else {
+
+                //Dá reset e close no Modal form
+                $('#addClient form :input').val("");
+                $('#addClient').modal('toggle');
+                $scope.alerts[0].show = true;
+            }
+        });
+    };
+
+
+    //Antigo Cliente
+    let oldClient;
+    //Editar Cliente
+    $scope.edsubmit = function () {
+        //Copia as informações todas do oldClient para um novo Cliente incluindo as informações inalteraveis (Pkanos de treino, etc)
+
+        let newClient = oldClient;
+
+        //Atribui as novas informções ao newClient
+        newClient.id = $scope.idclienteedit;
+        newClient.firstName = $scope.eduser.firstName;
+        newClient.lastName = $scope.eduser.lastName;
+        newClient.birthDate = formatDate($scope.eduser.birthDate);
+
+        //Formata a dob do user
+        let date = new Date(newClient.birthDate);
+        //Calcula a idade do user através da sua dob
+        newClient.age = getAge(date);
+
+        newClient.nif = $scope.eduser.nif;
+        newClient.heightInMeters = $scope.eduser.heightInMeters;
+        newClient.weightInKg = $scope.eduser.weightInKg;
+        newClient.imageUrl = $scope.eduser.imageUrl;
+
+        editarClient($http, newClient, $scope.idclienteedit, (response) => {
+            if (response) {
+                $scope.eduser = null;
+
+                //Dá reset e close no Modal form
+                $('#editarClient form :input').val("");
+                $('#editarClient').modal('toggle');
+                $scope.alerts[2].show = true;
+            } else {
+
+                //Dá reset e close no Modal form
+                $('#editarClient form :input').val("");
+                $('#editarClient').modal('toggle');
+                $scope.alerts[3].show = true;
+            }
+        })
+    };
+
 
     //Remove o cliente da Base de Dados
     $scope.rmCliente = function (id) {
@@ -46,7 +168,6 @@ app.controller("clientesCtrl", function ($scope, $http) {
         removeClient($http, id, (response) => {
             if (response) {
 
-                console.log("Removido com Sucesso!");
                 $scope.Clientes = $.grep($scope.Clientes, function (e) {
                     return e.id != id;
                 });
@@ -56,27 +177,26 @@ app.controller("clientesCtrl", function ($scope, $http) {
 
             } else {
 
-                console.log("Erro ao remover Cliente");
                 //Dá close no Modal from
                 $('#removercliente').modal('toggle');
-
-                //Gestão de Erros
-                //Validações
-
             }
         });
     };
 
-    
+
     //Abre um popup para colocar nova informação do Cliente
     $scope.edCliente = function (id) {
         $scope.idclienteedit = id;
+        oldClient = $scope.Clientes.find(x => x.id === $scope.idclienteedit);
+        $scope.eduser = oldClient;
     }
 
     //Abre um popup para confirmar a remoção do Cliente
     $scope.removeClient = function (id) {
         $scope.idclienteremove = id;
     }
+
+
 });
 
 
--- GymWeb/BackOffice/Angular/routes.js
+++ GymWeb/BackOffice/Angular/routes.js
@@ -20,6 +20,12 @@ app.config(function($routeProvider) {
     .when("/ticket/:id", {
         templateUrl : "Templates/ticket.html"
     })
+    .when("/listadeplanos",{ //Página com a lista de Planos de Exercicios
+        templateUrl : "Templates/404.htm"
+    })
+    .when("/listadeexercicios",{ //Página com a lista de Exercicios
+        templateUrl : "Templates/404.htm"
+    })
     .otherwise({
         templateUrl : "Templates/404.htm"
     })
