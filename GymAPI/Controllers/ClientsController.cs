7b898222eb5a4ae9528f05135c11dfa28cf06c5a
Diogo Xavier <diogoxavierpinto@gmail.com>
2018-12-10 21:18:24 +0000

    Remover async desnecessário no ClientsController

--- GymAPI/Controllers/ClientsController.cs
+++ GymAPI/Controllers/ClientsController.cs
@@ -234,7 +234,7 @@ namespace GymAPI
         // POST api/clients/{id}/plan
         [HttpPost("{id}/plan")]
         [AllowAnonymous]
-        public async Task<ActionResult> SwitchTrainingPlan(long id, [FromBody] SwitchPlanDAO planIdObj)
+        public ActionResult SwitchTrainingPlan(long id, [FromBody] SwitchPlanDAO planIdObj)
         {
             var _isAdmin = _authService.CheckIfAdmin(User);
             var _isTrainer = _authService.CheckIfTrainer(User);
