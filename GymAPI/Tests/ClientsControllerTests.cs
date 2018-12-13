0b6ba078fc1166901237bcd391c5b7deea1158e4
Diogo Xavier <diogoxavierpinto@gmail.com>
2018-12-13 01:13:55 +0000

    Modificar testes

--- GymAPI/Tests/ClientsControllerTests.cs
+++ GymAPI/Tests/ClientsControllerTests.cs
@@ -13,20 +13,29 @@ namespace Tests
     public class ClientsControllerTests : ControllerTestBase
     {
         private readonly string API_ENDPOINT = "/api/clients/";
-        private readonly HttpClient _client;
-        private readonly HttpClient _clientAdmin;
 
         public ClientsControllerTests()
         {
-            _client = base.GetHttpClient();
-            _clientAdmin = base.GetHttpClientWithAuthAdmin();
+            base.LoadUsers();
         }
 
-        [Fact]
-        public async Task GetClientsUnauthorized()
+        [InlineData("", "")]     // "api/clients"
+        [InlineData("", "1")]    // "api/clients/1"
+        [Theory]
+        public async Task GetWithoutAuth(string endpoint, string id)
         {
+            var endpointEnding = "";
+            if (id != "")
+            {
+                endpointEnding = endpoint + "/" + id;
+            }
+            else
+            {
+                endpointEnding = endpoint;
+            }            
+            
             // Should return 401 - Unauthorized
-            var response = await _client.GetAsync(API_ENDPOINT);
+            var response = await base.NoAuth.GetAsync(API_ENDPOINT + endpoint);
             Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
         }
 
@@ -34,14 +43,13 @@ namespace Tests
         public async Task GetClients()
         {   
             // Should return 200 - Successful
-            var response = await _clientAdmin.GetAsync(API_ENDPOINT);
+            var response = await base.Admin.GetAsync(API_ENDPOINT);
             response.EnsureSuccessStatusCode();
 
             // Should return an empty list
             var stringResponse = await response.Content.ReadAsStringAsync();
             var result = JsonConvert.DeserializeObject<List<Client>>(stringResponse);
             Assert.Empty(result);
-    
         }
     }
     
--- GymAPI/Tests/TestHttpClient.cs
+++ GymAPI/Tests/TestHttpClient.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using GymAPI;
+using GymAPI.DAOs;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Newtonsoft.Json;
@@ -10,6 +12,12 @@ namespace Tests
 {
     public class ControllerTestBase
     {
+        public HttpClient NoAuth { get; set; }
+        public HttpClient Admin { get; set; }
+        public HttpClient Client1 { get; set; }
+        public HttpClient Client2 { get; set; }
+        public HttpClient StaffMember { get; set; }
+        public HttpClient Trainer { get; set; }
 
         protected HttpClient GetHttpClient()
         {
@@ -24,10 +32,10 @@ namespace Tests
             return client;
         }
         
-        protected HttpClient GetHttpClientWithAuthAdmin()
+        protected HttpClient GetHttpClientWithAuth(string username, string password)
         {
             var client = GetHttpClient();
-            var response = client.PostAsync("/api/auth/login/", new StringContent(" { 'username': 'admin', 'password': 'Password123' } ", Encoding.UTF8, "application/json"));
+            var response = client.PostAsync("/api/auth/login/", new StringContent(" { 'username': '" + username + "', 'password': '" + password + "' } ", Encoding.UTF8, "application/json"));
             response.Wait();
 
             var authTokenObj = response.Result.Content.ReadAsStringAsync();
@@ -42,6 +50,13 @@ namespace Tests
         private class BearerToken
         {
             public string Token { get; set; }
+        }
+
+        protected void LoadUsers()
+        {   
+            var httpClient = GetHttpClient();
+            NoAuth = httpClient;
+            Admin = GetHttpClientWithAuth("admin", "Password123");
         }
     }
 }
