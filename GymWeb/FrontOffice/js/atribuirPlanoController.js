4e495e5b657866d3e16afb03cde9573a0dbaab51
Paulo Coelho <paulocoelho.98@hotmail.com>
2018-12-14 17:35:04 +0000

    Adicionar paginação à página de atribuir planos. #108

--- GymWeb/FrontOffice/js/atribuirPlanoController.js
+++ GymWeb/FrontOffice/js/atribuirPlanoController.js
@@ -1,5 +1,5 @@
 import { getAllClients, getPlanosTreinoById, getPlanosTreino, changeClientPlan } from './pedidos.js'
-import { checkLogin } from './myutil.js'
+import { checkLogin, paginationSplitInChuncks, paginationOnDocumentReady, paginationSetPage } from './myutil.js'
 
 // Controller da página de atribuir planos
 app.controller('atribuirPlanoCtrl', function ($scope, $http, $rootScope) {
@@ -22,6 +22,15 @@ app.controller('atribuirPlanoCtrl', function ($scope, $http, $rootScope) {
         $rootScope.$broadcast('show-window', 'true');
 
         let listClients = [];
+
+        // Lista de equipamentos divididas em chuncks
+        let clientsChuncks = [];
+
+        // Quantidade de equipamentos por chunck
+        let elementsPerChunck = 5;
+
+        // Página atual da lista de equipamentos
+        let currentPage = 0;
 
         // Obter lista de todos os clients
         getAllClients($http, (response) => {
@@ -46,6 +55,7 @@ app.controller('atribuirPlanoCtrl', function ($scope, $http, $rootScope) {
                         });
                     }
                 }
+
             } else {
 
             }
@@ -53,7 +63,33 @@ app.controller('atribuirPlanoCtrl', function ($scope, $http, $rootScope) {
             // Atualiza a vista do utilizador
             $scope.clients = listClients;
 
+            atualizarPaginas();
+
         });
+
+        // Quando clica no numero de uma página
+        $scope.setPage = function (page) {
+
+            // currentPage recebe a página selecionada
+            currentPage = page;
+
+            // Atualiza a vista com os equipamentos dessa página
+            $scope.clients = clientsChuncks[page];
+
+            // Atualiza a vista dos botões da paginação (clicáveis, desativados, etc)
+            paginationSetPage(clientsChuncks, page);
+
+        }
+
+        // Se clicar no botão de página anterior, vai buscar a página atual a decrementa um
+        $scope.previousPage = function () {
+            $scope.setPage(currentPage - 1);
+        }
+
+        // Se clicar no botão de próxima página, vai buscar a página atual a incrementa um
+        $scope.nextPage = function () {
+            $scope.setPage(currentPage + 1);
+        }
 
         // Obtem todos os planos existentes
         getPlanosTreino($http, (planos) => {
@@ -74,7 +110,27 @@ app.controller('atribuirPlanoCtrl', function ($scope, $http, $rootScope) {
                 if (listClients[i].id === clientId) {
 
                     listClients[i].trainingPlanId = planId;
+                    
+                     // Obter qual o chunck que se localiza o exercicio editado
+                     let chunckNumber = Math.floor(i / elementsPerChunck);
 
+                     // Dentro do chunck obter a posição onde se localiza o exercicio editado
+                     let chunckPosition = i % elementsPerChunck;
+
+                     // Alterar o plano de treino do cliente
+                     getPlanosTreinoById($http, listClients[i].trainingPlanId, (plan) => {
+
+                        if (plan) {
+
+                            listClients[i].trainingPlan = plan.data;
+
+                            clientsChuncks[chunckNumber][chunckPosition].trainingPlan = plan.data;
+
+                        } else {
+
+                        }
+                    });
+                 
                     // Objeto a enviar no body do pedido
                     let obj = {planId: planId};
 
@@ -107,5 +163,35 @@ app.controller('atribuirPlanoCtrl', function ($scope, $http, $rootScope) {
                 }
             }
         }
+
+        /**
+         * Atualizar a paginação, separar em chuncks e criar as páginas
+         */
+        function atualizarPaginas() {
+
+            // Divide o array de exercicios em chuncks e retorna um 
+            // objeto representativo de cada página(nº de chuncks)
+            let pagination = paginationSplitInChuncks(listClients, elementsPerChunck);
+
+            // Define o array de chuncks
+            clientsChuncks = pagination.arrayChuncks;
+
+            // Lista com tantos elementos quanto o numero de chuncks
+            $scope.numberPages = pagination.numberOfPages;
+
+            // Atualiza a vista
+            $scope.clients = clientsChuncks[0];
+
+            // Quando a página estiver pronta, coloca a pagina 0 como selecionada
+            // e coloca a pagina anterior como disabled
+            $(document).ready(function () {
+
+                // Atualiza a vista dos botões da paginação (clicáveis, desativados, etc)
+                paginationOnDocumentReady(clientsChuncks);
+
+            });
+
+        }
+
     }
 });
